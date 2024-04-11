6ac65f10b255c7b0d5f1f06ee3f0f26a58762a51
Heungbin Oh <hoh20@myseneca.ca>
2024-04-10 21:38:43 -0400

    updating all file

---
+++ script.js
@@ -0,0 +1,79 @@
+const body = document.body;
+
+const btnHamburger = document.querySelector(".fa-bars");
+
+const addThemeClass = (bodyClass, btnClass) => {
+  body.classList.add(bodyClass);
+  btnTheme.classList.add(btnClass);
+};
+
+const getBodyTheme = localStorage.getItem("portfolio-theme");
+const getBtnTheme = localStorage.getItem("portfolio-btn-theme");
+
+addThemeClass(getBodyTheme, getBtnTheme);
+
+const setTheme = (bodyClass, btnClass) => {
+  body.classList.remove(localStorage.getItem("portfolio-theme"));
+  btnTheme.classList.remove(localStorage.getItem("portfolio-btn-theme"));
+
+  addThemeClass(bodyClass, btnClass);
+
+  localStorage.setItem("portfolio-theme", bodyClass);
+  localStorage.setItem("portfolio-btn-theme", btnClass);
+};
+
+
+btnTheme.addEventListener("click", toggleTheme);
+
+const displayList = () => {
+  const navUl = document.querySelector(".nav__list");
+
+  if (btnHamburger.classList.contains("fa-bars")) {
+    btnHamburger.classList.remove("fa-bars");
+    btnHamburger.classList.add("fa-times");
+    navUl.classList.add("display-nav-list");
+  } else {
+    btnHamburger.classList.remove("fa-times");
+    btnHamburger.classList.add("fa-bars");
+    navUl.classList.remove("display-nav-list");
+  }
+};
+
+btnHamburger.addEventListener("click", displayList);
+
+document.getElementById("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  let values = {};
+  Object.keys(event.target.elements).map((key) => {
+    if (isNaN(key)) {
+      values[key] = event.target.elements[key].value;
+    }
+  });
+
+  fetch("https://httpbin.org/post", {
+    method: "POST",
+    body: JSON.stringify(values),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(JSON.stringify(data, null, 2));
+      alert("Successfully submitted!");
+    })
+    .catch((err) => {
+      alert(`Fail to proceed! Please, try again`);
+    });
+});
+
+document.getElementById("question").addEventListener("change", () => {
+  document.getElementById("rate").style = "display: none;";
+});
+
+document.getElementById("comment").addEventListener("change", () => {
+  document.getElementById("rate").style = "display: none;";
+});
+
+document.getElementById("hiring").addEventListener("change", () => {
+  document.getElementById("rate").style = "display: grid;";
+});
+
+
